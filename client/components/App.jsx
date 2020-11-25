1ed3cb69d037db28eb3c70515320d8f4efc339a8
htense <hortenserothery@gmail.com>
2020-11-26 00:16:55 +1300

    initial

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
-import Nav from './Nav.jsx'
 import Home from './Home.jsx'
-import Header from './Header.jsx'
 
 
 
@@ -11,8 +9,6 @@ const App = () => {
   return (
     <>  
       <Router>
-      <Route path='/' component={Header} />
-      <Route path='/' component={Nav} />
       <Route exact path='/' component={Home}/>
       </Router>
     </>
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,30 +1,81 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 
 const Home = () => {
   return (
-    <div className= 'home-div'>
+<>
+<br/><br/><br/>
+ 
+      <div className='walks-wrapper'>
+      
+        <div className='each-walk'>
+          <div className= 'walk-image-and-title'>
+              <img className='walk-image'src='https://lh3.googleusercontent.com/proxy/eUbikXG0umAQAxlPJOZs_r8aQxq6EOnC6c00S_xgoqXlcgjCp7MVpyHgFrpYctmaVwGOX7wwdHp0FFdV5WJp9bg8QgZj3jshPlvr8UHeSyk8K53CtzsAyMUzixLzoWMK6waqKZnRwdxMdip35ak' alt="description"/>
 
-    <div className= 'browser-div' >
-    <img src="/images/browser.png" alt="browser"/>
+              <div className= 'walk-title-div'>
+              <span className= 'walk-title' >OTARI WILTONS BUSH</span>
+              <div><h5>Off Mairangi road</h5></div>
+              </div>
+          </div>
+          <div className='walk-info'>
+              <div className='walk-detail'><h4>Doggy friendly</h4></div>
+              <div className='walk-detail'><h4>⭐ ⭐ ⭐</h4></div>
+              <button className='text-btn'><h4>Button</h4></button>
+          </div>
+         </div> {/*end of each walk div*/}
 
-    <div className= 'buttons-div'>
-      <button className='text-btn'><h3>1. Clone you repo</h3><p>click here</p></button>
-      <button className='text-btn'><h3>4. Merge?</h3><p>u sure? have you done step 3?</p></button>
-      <button className='text-btn'><h3>4. again Merge for sure</h3><p>click here</p></button>
-      </div>
-      </div>
+         <div className='each-walk'>
+          <div className= 'walk-image-and-title'>
+              <img className='walk-image'src='https://www.doc.govt.nz/globalassets/images/places/northland/waipoua-forest/te-matua-ngahere-walk/walkway-1200.jpg' alt="description"/>
 
-    <div className= 'laptop-div'>
-    <img src="/images/laptop.png" alt="laptop"/>
+              <div className= 'walk-title-div'>
+              <span className= 'walk-title'>SNEAKY LITTLE TRACK</span>
+              <div><h5>Actually in Featherston</h5></div>
+              </div>
+          </div>
+          <div className='walk-info'>
+              <div className='walk-detail'><h4>Doggy friendly</h4></div>
+              <div className='walk-detail'><h4>⭐ ⭐ ⭐ ⭐ ⭐ ⭐ ⭐</h4></div>
+              <button className='text-btn'><h4>Button</h4></button>
+          </div>
+         </div> {/*end of each walk div*/}
+          
+         <div className='each-walk'>
+          <div className= 'walk-image-and-title'>
+              <img className='walk-image'src='https://i.pinimg.com/originals/da/5a/19/da5a19b584d9acc95f9984dc5061239c.jpg' alt="description"/>
 
-    <div className= 'buttons-div'>
-      <button className='text-btn'><h3>2. Make a Branch</h3><p>click here</p></button>
-      <button className='text-btn'><h3>3. Push your branch</h3><p>click here</p></button>
-      </div>
-      </div>
-     </div>
+              <div className= 'walk-title-div'>
+              <span className= 'walk-title' >ARO WALK 45</span>
+              <div><h5>Hidden in arvo valley</h5></div>
+              </div>
+          </div>
+          <div className='walk-info'>
+              <div className='walk-detail'><h4>Doggy friendly</h4></div>
+              <div className='walk-detail'><h4>⭐ ⭐ ⭐</h4></div>
+              <button className='text-btn'><h4>Button</h4></button>
+          </div>
+         </div> {/*end of each walk div*/}
+
+         <div className='each-walk'>
+          <div className= 'walk-image-and-title'>
+              <img className='walk-image'src='https://resources.stuff.co.nz/content/dam/images/1/t/j/s/s/5/image.related.StuffLandscapeSixteenByNine.710x400.1zn2qp.png/1603139047062.jpg' alt="description"/>
+
+              <div className= 'walk-title-div'>
+              <span className= 'walk-title' >THE JUNGLE TRACK</span>
+              <div><h5>Newtown shops</h5></div>
+              </div>
+          </div>
+          <div className='walk-info'>
+              <div className='walk-detail'><h4>Doggy friendly</h4></div>
+              <div className='walk-detail'><h4>⭐ ⭐ ⭐</h4></div>
+              <button className='text-btn'><h4>Button</h4></button>
+          </div>
+         </div> {/*end of each walk div*/}
+
+
+      </div>{/*end of walk wrapper vdiv*/}
+   
+    </>
   )
 }
 
